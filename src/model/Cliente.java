fc81a7b0ba9d7234d3e976f60cbd0238068ba877
Edmar Filho <109600197+edybit@users.noreply.github.com>
2024-09-24 20:09:51 -0300

    update cliente e funcionário

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,2 +1,75 @@
-public class Cliente {
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Cliente implements Serializable {
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String endereco;
+
+    public Cliente(String nome, String cpf, String telefone,String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.endereco=endereco;
+    }
+
+    // Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", endereco='" + endereco + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(cpf, cliente.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf);
+    }
 }
+
--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,2 +1,63 @@
-public class Funcionario {
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Funcionario implements Serializable {
+    private String nome;
+    private String cpf;
+    private String cargo;
+
+    public Funcionario(String nome, String cpf, String cargo) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.cargo = cargo;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Funcionario that = (Funcionario) o;
+        return Objects.equals(cpf, that.cpf);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf);
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", cargo='" + cargo + '\'' +
+                '}';
+    }
 }
