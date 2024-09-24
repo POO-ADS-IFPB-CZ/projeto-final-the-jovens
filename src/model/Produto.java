3e7da568b124a0e9d38b57e4ec5227b0a77bdb42
Gabriel da Silva Monteiro <113948429+GabrielSilva15@users.noreply.github.com>
2024-09-24 19:58:23 -0300

    Adicionando classe produto

---
+++ src/model/Produto.java
@@ -0,0 +1,85 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Produto implements Serializable {
+    private String nome;
+    private  String marca;
+    private String codigo;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, String codigo, double preco, int quantidade,String marca) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.marca=marca;
+    }
+
+    // Getters and Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", marca='" + marca + '\'' +
+                ", codigo='" + codigo + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(codigo, produto.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+}
