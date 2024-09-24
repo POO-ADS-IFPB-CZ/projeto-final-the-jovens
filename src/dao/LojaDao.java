e4250ae29191e42efee3cf87cf29c9fa699e5e2d
Marcos Viana <marcostuc00@gmail.com>
2024-09-24 19:43:31 -0300

    Criando classe loja e dao

--- src/dao/LojaDao.java
+++ src/dao/LojaDao.java
@@ -1,2 +1,54 @@
+import java.io.*;
+import java.util.List;
+import model.Loja;
+import model.Cliente;
+import model.Produto;
+import model.Funcionario;
+
 public class LojaDao {
+
+    private  File arquivo;
+
+    public LojaDao(){
+        arquivo = new File("loja.ser");
+
+        if(!arquivo.exists()){
+            try {
+                arquivo.createNewFile();
+            }catch (IOException e){
+                System.out.println("Falha ao criar o Arquivo");
+            }
+        }
+    }
+
+    public Loja getLoja(){
+        try {
+            FileInputStream fileInputStream = new FileInputStream(arquivo);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            Loja loja = (Loja) objectInputStream.readObject();
+            return loja;
+        }catch (FileNotFoundException e){
+            System.out.println("Arquivo não encontrado");
+        }catch (IOException e){
+            System.out.println("Falha ao ler arquivo!");
+        }catch (ClassNotFoundException e){
+            System.out.println("Falha ao ler arquivo!");
+        }
+        return new Loja();
+    }
+
+    public boolean salvarLoja(Loja loja){
+        try{
+            FileOutputStream outputStream = new FileOutputStream(arquivo);
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+            objectOutputStream.writeObject(loja);
+            return true;
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado");
+        } catch (IOException e) {
+            System.out.println("Falha ao escrever no arquivo");
+        }
+        return false;
+    }
+
 }
--- src/model/Loja.java
+++ src/model/Loja.java
@@ -1,2 +1,84 @@
-public class Loja {
+package model;
+import model.Cliente;
+import model.Funcionario;
+import model.Produto;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Loja implements Serializable {
+
+    private  String nome;
+    private List<Cliente> clientes;
+    private List<Funcionario> funcionarios;
+    private List<Produto> produtos;
+
+    public Loja() {
+        this.nome="The Jovens";
+        this.clientes = new ArrayList<>();
+        this.funcionarios = new ArrayList<>();
+        this.produtos = new ArrayList<>();
+    }
+
+    // Métodos para Clientes
+    public void adicionarCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
+    public void removerCliente(Cliente cliente) {
+        clientes.remove(cliente);
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    // Métodos para Funcionários
+    public void adicionarFuncionario(Funcionario funcionario) {
+        funcionarios.add(funcionario);
+    }
+
+    public void removerFuncionario(Funcionario funcionario) {
+        funcionarios.remove(funcionario);
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    // Métodos para Produtos
+    public void adicionarProduto(Produto produto) {
+        produtos.add(produto);
+    }
+
+    public void removerProduto(Produto produto) {
+        produtos.remove(produto);
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    @Override
+    public String toString() {
+        return "Loja{" +
+                "nome='" + nome + '\'' +
+                ", clientes=" + clientes +
+                ", funcionarios=" + funcionarios +
+                ", produtos=" + produtos +
+                '}';
+    }
 }
